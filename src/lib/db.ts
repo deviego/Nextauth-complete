43dc882e646eb3f4edc0707c72792179bc095177
deviego <deviego4@gmail.com>
2023-09-28 18:55:52 -0300

    first commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  })
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
@@ -12,7 +14,7 @@ const globalForPrisma = globalThis as unknown as {
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-export default prisma
+
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
